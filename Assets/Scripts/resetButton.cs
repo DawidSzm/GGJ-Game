efdc63178b6b70f8dbe7c07530aad93497e9ccfd
Ayton Nijboer <ayton-nijboer@hotmail.com>
2022-01-28 22:55:17 +0100

    gitflow-feature-stash: reset

---
+++ Assets/Scripts/resetButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetButton : MonoBehaviour
+{
+    public Object activeScene;
+    void Start()
+    {
+        //Debug.Log("LoadSceneA");
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
